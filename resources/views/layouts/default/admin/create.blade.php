3f53779efc76e116a0e357debf24477f71b38e35
Retype GitHub Action <retype@object.net>
2022-12-30 14:48:24 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/views/layouts/default/admin/create.blade.php
+++ resources/views/layouts/default/admin/create.blade.php
@@ -1,29 +1,20 @@
 @extends('adm_theme::layouts.app')
 @php
-    //dddx(get_defined_vars());
-    $last_container = last($containers);
     if (!is_object($row)) {
         return '';
     }
     $fields = $_panel->getFields(['act' => 'edit']);
 @endphp
 @section('content')
-    @component('ui::components.crud', get_defined_vars())
-        @slot('content')
-            <x-include-view view="topbar" />
-            {{-- prima di rimettere sto schifo spiegatemelo
-            <h1>Crea {{ $_panel_name }}</h1> --}}
-            {{--
-            <x-alerts.error :errors="$errors" />
-            --}}
-            {!! Form::bsOpenPanel($_panel, 'store') !!}
-            <div class="row">
-                @foreach ($fields as $field)
-                    {!! Theme::inputHtml($field, $row) !!}
-                @endforeach
-            </div>
-            {{ Form::bsSubmit('Salva') }}
-            {!! Form::close() !!}
-        @endslot
-    @endcomponent
+    <x-section type="crud">
+        <x-include-view view="topbar" />
+        {!! Form::bsOpenPanel($_panel, 'store') !!}
+        <div class="row">
+            @foreach ($fields as $field)
+                {!! Theme::inputHtml($field, $row) !!}
+            @endforeach
+        </div>
+        {{ Form::bsSubmit('Salva') }}
+        {!! Form::close() !!}
+    </x-section>
 @endsection
