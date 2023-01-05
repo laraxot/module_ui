d369f40ac93e70d020e54f745036b129ed116542
Retype GitHub Action <retype@object.net>
2023-01-05 16:59:39 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/views/layouts/default/admin/show.blade.php
+++ resources/views/layouts/default/admin/show.blade.php
@@ -1,7 +1,7 @@
 @extends('adm_theme::layouts.app')
 @section('content')
     @php
-        $fields = $_panel->fields();
+        $fields = $_panel->getFields();
     @endphp
     <table>
         @foreach ($fields as $k => $v)
