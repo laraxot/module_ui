4458ec766a05d49fa6ec410e4ea0b50c6774e16d
marco xot <marco.sottana@gmail.com>
2023-01-12 14:48:20 +0100

    .

---
+++ View/Components/Col.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\UI\View\Components;
+
+use Illuminate\View\Component;
+
+/**
+ * Class Col.
+ */
+class Col extends Component {
+    public int $size;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct(int $size) {
+        $this->size = $size;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): \Illuminate\Contracts\Support\Renderable {
+        return view()->make('ui::components.col');
+    }
+}
