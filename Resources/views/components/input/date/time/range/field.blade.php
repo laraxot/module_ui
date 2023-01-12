6e37a9dd85dd01f0fe9ed39e20456d2dbff36a5e
marco xot <marco.sottana@gmail.com>
2023-01-12 14:31:26 +0100

    .

--- Resources/views/components/input/date/time/range/field.blade.php
+++ Resources/views/components/input/date/time/range/field.blade.php
@@ -17,47 +17,46 @@
         $(function() {
             moment.locale('it');
 
-           var start = moment($('#{{ Str::slug($name) }} [name="date_from"]').val());
-           if (!start.isValid()) {
-               start = moment().subtract(29, 'days').set('hour', 0).set('minute', 0).set('second', 0);
-           }
-           var end = moment($('#{{ Str::slug($name) }} [name="date_to"]').val());
-           if (!end.isValid()) {
-               end = moment().set('hour', 23).set('minute', 59).set('second', 59);
-           }
+            var start = moment($('#{{ Str::slug($name) }} [name="date_from"]').val());
+            if (!start.isValid()) {
+                start = moment().subtract(29, 'days').set('hour', 0).set('minute', 0).set('second', 0);
+            }
+            var end = moment($('#{{ Str::slug($name) }} [name="date_to"]').val());
+            if (!end.isValid()) {
+                end = moment().set('hour', 23).set('minute', 59).set('second', 59);
+            }
 
 
-           function cb(start, end) {
-               $('#{{ Str::slug($name) }} span').html(start.format('D MMM YYYY HH:mm') + ' - ' + end.format(
-                   'D MMM YYYY HH:mm'));
-               $('#{{ Str::slug($name) }} [name="date_from"]').val(start.format('yyyy-MM-DDTHH:mm:ss'))
-               $('#{{ Str::slug($name) }} [name="date_to"]').val(end.format('yyyy-MM-DDTHH:mm:ss'))
-               //console.log("{{ $name }}");
-               @this.emit('setFormDataValueEvent', "{{ $name }}_from", start.format(
-                   'yyyy-MM-DDTHH:mm:ss'));
-               @this.emit('setFormDataValueEvent', "{{ $name }}_to", end.format('yyyy-MM-DDTHH:mm:ss'));
-               @this.emit('updatedFormDataRowsEvent', @this.form_data);
-           }
+            function cb(start, end) {
+                $('#{{ Str::slug($name) }} span').html(start.format('D MMM YYYY HH:mm') + ' - ' + end.format(
+                    'D MMM YYYY HH:mm'));
+                $('#{{ Str::slug($name) }} [name="date_from"]').val(start.format('yyyy-MM-DDTHH:mm:ss'))
+                $('#{{ Str::slug($name) }} [name="date_to"]').val(end.format('yyyy-MM-DDTHH:mm:ss'))
+                //console.log("{{ $name }}");
+                @this.emit('setFormDataValueEvent', "{{ $name }}_from", start.format(
+                    'yyyy-MM-DDTHH:mm:ss'));
+                @this.emit('setFormDataValueEvent', "{{ $name }}_to", end.format('yyyy-MM-DDTHH:mm:ss'));
+                @this.emit('updatedFormDataRowsEvent', @this.form_data);
+            }
 
 
-           $('#{{ Str::slug($name) }}').daterangepicker({
-               timePicker: true,
-               timePicker24Hour: true,
-               startDate: start,
-               endDate: end,
-               ranges: {
-                   'Today': [moment(), moment()],
-                   'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-                   'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-                   'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-                   'This Month': [moment().startOf('month'), moment().endOf('month')],
-                   'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1,
-                       'month').endOf('month')]
-               }
-           }, cb);
+            $('#{{ Str::slug($name) }}').daterangepicker({
+                timePicker: true,
+                timePicker24Hour: true,
+                startDate: start,
+                endDate: end,
+                ranges: {
+                    'Today': [moment(), moment()],
+                    'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                    'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                    'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                    'This Month': [moment().startOf('month'), moment().endOf('month')],
+                    'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1,
+                        'month').endOf('month')]
+                }
+            }, cb);
 
-           cb(start, end);
+            cb(start, end);
         });
     </script>
 @endpush
-
