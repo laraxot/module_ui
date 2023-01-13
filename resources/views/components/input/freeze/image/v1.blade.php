513931b3ee481146c05c85d092feca63a40eebf6
Retype GitHub Action <retype@object.net>
2023-01-13 10:35:25 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

---
+++ resources/views/components/input/freeze/image/v1.blade.php
@@ -0,0 +1,1 @@
+<img src="{{ asset($value) }}" style="height:100px"/>
--- resources/views/components/input/freeze/null/v1.blade.php
+++ resources/views/components/input/freeze/null/v1.blade.php
@@ -1,1 +1,8 @@
-{{ $value }}
+{{ print_r($value,true) }}
+@if(is_iterable($field->fields))
+    @foreach ($field->fields as $v)
+        <b>{{ $v->name }}</b>: <x-input.freeze :field="$v" :row="$row" /><br/>
+    @endforeach
+@else
+    {{-- dddx(['value'=>$value,'field'=>$field,'row'=>$row]) --}}
+@endif
