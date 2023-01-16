858d65a92d8576e0e45ce1b82074c71d547b9f14
Retype GitHub Action <retype@object.net>
2023-01-16 10:15:02 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/views/components/topbar/v1.blade.php
+++ resources/views/components/topbar/v1.blade.php
@@ -12,6 +12,7 @@
                 <!-- Actions -->
                 <div class="col-sm-auto col-12 mt-4 mt-sm-0">
                     <div class="hstack gap-2 justify-content-sm-end">
+                        {{--
                         <a href="#modalExport" class="btn btn-sm btn-neutral border-base" data-bs-toggle="modal">
                             <span class="pe-2">
                                 <i class="bi bi-people-fill"></i>
@@ -24,21 +25,44 @@
                             </span>
                             <span>Create</span>
                         </a>
+                        --}}
+                        {{--
+                        @foreach ($_panel->containerActions() as $act)
+                        
+                        @php
+                            try {
+                                echo $act->btnHtml();
+                            } catch (\Exception $e) {
+                                //dddx(['act'=>$act,'e'=>$e]);
+                            }
+                        @endphp
+                        @endforeach
+                        --}}
+                        <x-panel.actions.buttons :acts="$_panel->containerActions()" />
+                        
                     </div>
                 </div>
             </div>
+
+            @foreach($_panel->getTabs() as $lvl)
             <!-- Nav -->
             <ul class="nav nav-tabs overflow-x border-0">
+                @foreach($lvl as $v)
+                
                 <li class="nav-item">
-                    <a href="#" class="nav-link active">View all</a>
+                    <a href="{{ $v->url }}" class="nav-link {{$v->active?'active':'' }} ">{{ $v->title }}</a>
                 </li>
+                @endforeach
+                {{--
                 <li class="nav-item">
                     <a href="#" class="nav-link">Most recent</a>
                 </li>
                 <li class="nav-item">
                     <a href="#" class="nav-link">Popular</a>
                 </li>
+                --}}
             </ul>
+            @endforeach 
         </div>
     </div>
 </header>
