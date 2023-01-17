8493b2d4718a2fd84da5e8b817f1a4b7dc15d7bb
Retype GitHub Action <retype@object.net>
2023-01-17 11:27:11 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/views/layouts/default/admin/index_clever.blade.php
+++ resources/views/layouts/default/admin/index_clever.blade.php
@@ -1,5 +1,38 @@
 @extends('adm_theme::layouts.app')
 @section('content')
+<<<<<<< HEAD
+    <x-topbar />
+    <x-pagination :rows="$rows" />
+    <!-- Table -->
+    <div class="card">
+        <div class="card-header border-bottom d-flex align-items-center">
+            <h5 class="me-auto">All projects</h5>
+            <div class="dropdown">
+                <a class="text-muted" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true"
+                    aria-expanded="false">
+                    <i class="bi bi-three-dots-vertical"></i>
+                </a>
+                <div class="dropdown-menu">
+                    <a href="#!" class="dropdown-item">
+                        Action
+                    </a>
+                    <a href="#!" class="dropdown-item">
+                        Another action
+                    </a>
+                    <a href="#!" class="dropdown-item">
+                        Something else here
+                    </a>
+                </div>
+            </div>
+        </div>
+        <x-panel.crud :panel="$_panel" />
+
+        <div class="card-footer border-0 py-5">
+            <span class="text-muted text-sm">Showing 10 items out of 250 results found</span>
+        </div>
+    </div>
+@endsection
+=======
 <x-topbar />
 <!-- Table -->
 <div class="card">
@@ -63,3 +96,4 @@
 	--}}
 </div>
 @endsection
+>>>>>>> 88c6711d233db4fc85401a852f577a9b214d54d5
