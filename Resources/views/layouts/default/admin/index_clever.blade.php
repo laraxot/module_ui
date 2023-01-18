31b0dd62444136194ff3f83f5562cc23aa53d88d
marco76tv <marco.sottana@gmail.com>
2023-01-18 09:28:51 +0100

    .

--- Resources/views/layouts/default/admin/index_clever.blade.php
+++ Resources/views/layouts/default/admin/index_clever.blade.php
@@ -1,38 +1,5 @@
 @extends('adm_theme::layouts.app')
 @section('content')
-<<<<<<< HEAD
-    <x-topbar />
-    <x-pagination :rows="$rows" />
-    <!-- Table -->
-    <div class="card">
-        <div class="card-header border-bottom d-flex align-items-center">
-            <h5 class="me-auto">All projects</h5>
-            <div class="dropdown">
-                <a class="text-muted" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true"
-                    aria-expanded="false">
-                    <i class="bi bi-three-dots-vertical"></i>
-                </a>
-                <div class="dropdown-menu">
-                    <a href="#!" class="dropdown-item">
-                        Action
-                    </a>
-                    <a href="#!" class="dropdown-item">
-                        Another action
-                    </a>
-                    <a href="#!" class="dropdown-item">
-                        Something else here
-                    </a>
-                </div>
-            </div>
-        </div>
-        <x-panel.crud :panel="$_panel" />
-
-        <div class="card-footer border-0 py-5">
-            <span class="text-muted text-sm">Showing 10 items out of 250 results found</span>
-        </div>
-    </div>
-@endsection
-=======
 <x-topbar />
 <!-- Table -->
 <div class="card">
@@ -96,4 +63,7 @@
 	--}}
 </div>
 @endsection
+<<<<<<< HEAD
 >>>>>>> 88c6711 (up)
+=======
+>>>>>>> 75be95a (up)
