365156611261387bb69ec7aa60356367950cb1c7
marco76tv <marco.sottana@gmail.com>
2023-01-18 10:59:18 +0100

    .

--- Resources/views/collective/fields/select/freeze.blade.php
+++ Resources/views/collective/fields/select/freeze.blade.php
@@ -4,8 +4,22 @@ $val='--';
 $coll=collect($field->options);
 if(isset($field->value) && $field->value!="" && !is_object($field->value)){
     $val=$coll->get($field->value);
-}elseif(is_object($field->value) && is_object($field->value->row) ){
+}elseif(is_object($field->value) && isset($field->value->row) &&  is_object($field->value->row) ){
     $val=Panel::make()->get($field->value)->optionLabel($field->value->row);
+}elseif($field->value instanceof \Illuminate\Database\Eloquent\Model){
+    $val=Panel::make()->get($field->value)->optionLabel($field->value);
+}elseif($field->value instanceof \Illuminate\Support\Collection){
+    //dddx('collection');
+    $val='';
+    foreach($field->value as $v){
+        if($v instanceof \Illuminate\Database\Eloquent\Model){
+            $val.='['.Panel::make()->get($v)->optionLabel($v).']';
+        }else{
+            $val.='--';
+        }
+    }
+}else{
+    //dddx($field->value);
 }
 @endphp
 {{ $val }} 
