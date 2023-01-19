439a9201c8822dacfa4f4e1774ada2d931d1dd20
Retype GitHub Action <retype@object.net>
2023-01-19 14:50:18 +0000

    Refreshes Retype-generated documentation.
    Process triggered by dade1987.

--- resources/views/layouts/wizard.blade.php
+++ resources/views/layouts/wizard.blade.php
@@ -1,18 +1,4 @@
 <div>
-<<<<<<< Updated upstream
-    {{-- dddx([
-        //'previousStep'=>$this->previousStep(),
-        //'nextStep'=>$this->nextStep(),
-        'get_defined_vars'=>get_defined_vars(),
-        'methods'=>get_class_methods($this),
-        //'vars'=>get_class_vars($this),
-        'state'=>$this->state(),
-        'state_methods'=>get_class_methods($this->state()),
-        'state_current'=>$this->state()->currentStep(),
-        ]) 
-    --}}
-=======
->>>>>>> Stashed changes
     <ul class="nav nav-tabs overflow-x border-0">
         @foreach ($steps as $step)
             <li class="nav-item">
@@ -23,23 +9,6 @@
 
     @yield('content')
 
-<<<<<<< Updated upstream
-    {{--
-    <div>
-        @if(!$is_first)
-         <div wire:click="previousStep" class="btn btn-primary">
-            ⬅ Previous
-        </div>
-        @endif
-        @if(!$is_last)
-        <div wire:click="goNextStep" class="btn btn-primary">
-            Go to the next step
-        </div>
-        @endif
-    </div>
-    --}}
-=======
->>>>>>> Stashed changes
     <nav aria-label="Page navigation example">
         <ul class="pagination">
             <li class="page-item {{ $is_first ? 'disabled' : '' }}">
@@ -54,8 +23,4 @@
             </li>
         </ul>
     </nav>
-<<<<<<< Updated upstream
 </div>
-=======
-</div>
->>>>>>> Stashed changes
