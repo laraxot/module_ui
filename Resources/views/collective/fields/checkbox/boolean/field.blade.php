22850f6181b310d3b6f826a393681247372274fd
Davide Cavallini <davidecavallini1987@gmail.com>
2023-01-19 17:32:01 +0100

    .

--- Resources/views/collective/fields/checkbox/boolean/field.blade.php
+++ Resources/views/collective/fields/checkbox/boolean/field.blade.php
@@ -1,9 +1,9 @@
 @php
-$field = transFields(get_defined_vars());
-//provo a cambiare un po il layout per visualizzare meglio il checkbox
-//utilizzo un nuovo blade_component fatto a doc per i campi boolean
-//(ancora in test, nel caso cancellare il nuovo componente)
-//dddx($field->attributes);
+    $field = transFields(get_defined_vars());
+    //provo a cambiare un po il layout per visualizzare meglio il checkbox
+    //utilizzo un nuovo blade_component fatto a doc per i campi boolean
+    //(ancora in test, nel caso cancellare il nuovo componente)
+    //dddx($field->attributes);
 @endphp
 @component($blade_component, get_defined_vars())
     @slot('label')
