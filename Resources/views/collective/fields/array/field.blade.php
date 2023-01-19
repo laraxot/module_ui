04acd97e87f1186e709b2af1d102c03de941ff66
marco76tv <marco.sottana@gmail.com>
2023-01-19 18:30:45 +0100

    rebase

--- Resources/views/collective/fields/array/field.blade.php
+++ Resources/views/collective/fields/array/field.blade.php
@@ -20,5 +20,16 @@
     
 @endphp
 {{-- dddx($field->options) --}}
+<<<<<<< HEAD
 <livewire:input.arr.assoc :name="$name" :value="$val"></livewire:input.arr.assoc>
 >>>>>>> f532fb8 (up)
+=======
+@component($blade_component, get_defined_vars())
+    @slot('label')
+        {{ Form::label($name, $field->label, ['class' => 'control-label form-label']) }}
+    @endslot
+    @slot('input')
+        <livewire:input.arr.assoc :name="$name" :value="$val"></livewire:input.arr.assoc>
+    @endslot
+@endcomponent
+>>>>>>> 992e8c4 (up)
--- Resources/views/livewire/input/arr/assoc/v1.blade.php
+++ Resources/views/livewire/input/arr/assoc/v1.blade.php
@@ -1,5 +1,6 @@
 <div>
 <<<<<<< HEAD
+<<<<<<< HEAD
     <button type="button" class="btn btn-primary" wire:click="addArr">+</button>
     @foreach ($form_data[$name] as $key => $val)
         {{-- {{ dddx([$key, $val]) }} 
@@ -23,6 +24,11 @@
     <input type="text" wire:model="new_key" />
     <input type="text" wire:model="value" />
     <button class="btn btn-primary" wire:click="addArr()">+</button>
+=======
+    <input class="form-control" type="text" wire:model="new_key" />
+    <input class="form-control" type="text" wire:model="value" />
+    <button class="btn btn-primary" wire:click="addArr">+</button>
+>>>>>>> 992e8c4 (up)
     @foreach ($value as $key => $val)
         {{-- {{ dddx([$key, $val]) }} --}}
         <input type="text" name="{{ $name }}[]" wire:model="form_data.{{ $name }}.{{ $key }}">
