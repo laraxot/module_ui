0b8d08e58c4f59872a49364aab5a67b995957e1d
Retype GitHub Action <retype@object.net>
2023-01-19 17:31:45 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/views/collective/fields/array/field.blade.php
+++ resources/views/collective/fields/array/field.blade.php
@@ -2,6 +2,10 @@
     $field = transFields(get_defined_vars());
     
     $val = Form::getValueAttribute($name);
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> c65baac (.)
     if(!is_array($val)){
         $val=[];
     }
@@ -15,3 +19,20 @@
         <livewire:input.arr.assoc :name="$name" :value="$val"></livewire:input.arr.assoc>
     @endslot
 @endcomponent
+=======
+    
+@endphp
+{{-- dddx($field->options) --}}
+<<<<<<< HEAD
+<livewire:input.arr.assoc :name="$name" :value="$val"></livewire:input.arr.assoc>
+>>>>>>> f532fb8 (up)
+=======
+@component($blade_component, get_defined_vars())
+    @slot('label')
+        {{ Form::label($name, $field->label, ['class' => 'control-label form-label']) }}
+    @endslot
+    @slot('input')
+        <livewire:input.arr.assoc :name="$name" :value="$val"></livewire:input.arr.assoc>
+    @endslot
+@endcomponent
+>>>>>>> 992e8c4 (up)
--- resources/views/livewire/input/arr/assoc/v1.blade.php
+++ resources/views/livewire/input/arr/assoc/v1.blade.php
@@ -1,4 +1,9 @@
 <div>
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> c65baac (.)
     <button type="button" class="btn btn-primary" wire:click="addArr">+</button>
     @foreach ($form_data[$name] as $key => $val)
         {{-- {{ dddx([$key, $val]) }} 
@@ -16,6 +21,25 @@
             {{ $form_data[$name][$key]['k'] }} : {{ $form_data[$name][$key]['v'] }} <br/>
             <input type="text" name="{{ $name }}[{{ $form_data[$name][$key]['k'] }}]" value="{{ $form_data[$name][$key]['v'] }}" />
         </div>
+<<<<<<< HEAD
     @endforeach
     
+=======
+    <input type="text" wire:model="new_key" />
+    <input type="text" wire:model="value" />
+    <button class="btn btn-primary" wire:click="addArr()">+</button>
+=======
+    <input class="form-control" type="text" wire:model="new_key" />
+    <input class="form-control" type="text" wire:model="value" />
+    <button class="btn btn-primary" wire:click="addArr">+</button>
+>>>>>>> 992e8c4 (up)
+    @foreach ($value as $key => $val)
+        {{-- {{ dddx([$key, $val]) }} --}}
+        <input type="text" name="{{ $name }}[]" wire:model="form_data.{{ $name }}.{{ $key }}">
+    @endforeach
+>>>>>>> f532fb8 (up)
+=======
+    @endforeach
+    
+>>>>>>> c65baac (.)
 </div>
