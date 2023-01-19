ec25dd983d018713a8587ae8709fd16f8849e097
Retype GitHub Action <retype@object.net>
2023-01-19 09:27:31 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/views/layouts/default/admin/index_clever.blade.php
+++ resources/views/layouts/default/admin/index_clever.blade.php
@@ -1,14 +1,14 @@
 @extends('adm_theme::layouts.app')
 @section('content')
-<x-topbar />
-<!-- Table -->
-<div class="card">
-	<div class="card-header border-bottom d-flex align-items-center">
-		<h5 class="me-auto">
-			<span class="primary-color"><strong>{{ number_format($rows->total(), 0, ',', ' ') }}</strong></span>
-			{{ Str::plural($row->post_type ?? class_basename($row), $rows->total()) }}
-		</h5>
-        {{--  
+    <x-topbar />
+    <!-- Table -->
+    <div class="card">
+        <div class="card-header border-bottom d-flex align-items-center">
+            <h5 class="me-auto">
+                <span class="primary-color"><strong>{{ number_format($rows->total(), 0, ',', ' ') }}</strong></span>
+                {{ Str::plural($row->post_type ?? class_basename($row), $rows->total()) }}
+            </h5>
+            {{--  
 		<div class="dropdown">
 			<a class="text-muted" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true"
 				aria-expanded="false">
@@ -27,8 +27,8 @@
 			</div>
 		</div>
         --}}
-	</div>
-    {{--  
+        </div>
+        {{--  
 	<div class="px-4 py-4 border-top border-bottom d-flex flex-column flex-sm-row gap-3">
 		<div class="btn-group">
 			<a href="#" class="btn btn-sm btn-neutral text-primary" aria-current="page">View all</a>
@@ -55,11 +55,11 @@
 		</div>
 	</div>
     --}}
-	<x-panel.crud :panel="$_panel" />
-	{{--  
+        <x-panel.crud :panel="$_panel" />
+        {{--  
 	<div class="card-footer border-0 py-5">
 		<span class="text-muted text-sm">Showing 10 items out of 250 results found</span>
 	</div>
 	--}}
-</div>
+    </div>
 @endsection
