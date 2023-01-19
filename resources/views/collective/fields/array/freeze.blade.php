0970f1f5b2e0860a82cbfa73ddfb7e8cc2f0a0f7
Retype GitHub Action <retype@object.net>
2023-01-19 09:00:48 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/views/collective/fields/array/freeze.blade.php
+++ resources/views/collective/fields/array/freeze.blade.php
@@ -1,6 +1,9 @@
 @php
     $values=$field->value ?? [];
 @endphp
-@foreach($values as $value)
+@foreach($values as $key => $value)
+    {{--
     <span class="badge badge-xs">{{$value}}</span>
+    --}}
+    <span class="badge bg-soft-primary text-primary rounded-pill">{{ $key }}: {{ $value }}</span>
 @endforeach
