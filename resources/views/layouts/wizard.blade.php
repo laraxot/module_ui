18dd6247dcf00b59c40b18e7c76c9b24ee7c23a2
Retype GitHub Action <retype@object.net>
2023-01-19 10:50:11 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/views/layouts/wizard.blade.php
+++ resources/views/layouts/wizard.blade.php
@@ -2,10 +2,14 @@
     {{-- dddx([
         //'previousStep'=>$this->previousStep(),
         //'nextStep'=>$this->nextStep(),
-        //'methods'=>get_class_methods($this)
+        'get_defined_vars'=>get_defined_vars(),
+        'methods'=>get_class_methods($this),
+        //'vars'=>get_class_vars($this),
         'state'=>$this->state(),
         'state_methods'=>get_class_methods($this->state()),
-        ]) --}}
+        'state_current'=>$this->state()->currentStep(),
+        ]) 
+    --}}
     <ul class="nav nav-tabs overflow-x border-0">
         @foreach($steps as $step)
             <li class="nav-item">
@@ -16,13 +20,32 @@
 
     @yield('content')
 
+    {{--
     <div>
+        @if(!$is_first)
          <div wire:click="previousStep" class="btn btn-primary">
             ⬅ Previous
         </div>
-
+        @endif
+        @if(!$is_last)
         <div wire:click="goNextStep" class="btn btn-primary">
             Go to the next step
         </div>
+        @endif
     </div>
+    --}}
+    <nav aria-label="Page navigation example">
+        <ul class="pagination">
+          <li class="page-item {{$is_first?'disabled':''}}">
+            <a class="page-link" href="#" aria-label="Previous" wire:click="previousStep">
+              <span aria-hidden="true">&laquo;</span>
+            </a>
+          </li>
+          <li class="page-item {{$is_last?'disabled':''}}">
+            <a class="page-link" href="#" aria-label="Next" wire:click="goNextStep">
+              <span aria-hidden="true">&raquo;</span>
+            </a>
+          </li>
+        </ul>
+    </nav>
 </div>
