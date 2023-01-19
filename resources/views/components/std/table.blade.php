c6469bbe9c9688473f1827c54558e2ea9a74d979
Retype GitHub Action <retype@object.net>
2023-01-19 16:57:59 +0000

    Refreshes Retype-generated documentation.
    Process triggered by dade1987.

--- resources/views/components/std/table.blade.php
+++ resources/views/components/std/table.blade.php
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
 @props([
     'thead' => '',
     'tbody' => '',
 ])
-
-=======
->>>>>>> 88c6711 (up)
 <table class="table table-bordered">
     {!! $thead !!}
-
     {!! $tbody !!}
-<<<<<<< HEAD
 </table>
-=======
-</table>
->>>>>>> 88c6711 (up)
