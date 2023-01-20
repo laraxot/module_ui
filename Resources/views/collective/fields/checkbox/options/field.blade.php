ed7013a807f35676bc89881bf2ccdbc1d38fb112
marco xot <marco.sottana@gmail.com>
2023-01-20 18:17:19 +0100

    .

--- Resources/views/collective/fields/checkbox/options/field.blade.php
+++ Resources/views/collective/fields/checkbox/options/field.blade.php
@@ -1,15 +1,15 @@
 @php
-$field = transFields(get_defined_vars());
-$val = Form::getValueAttribute($name);
-$options=$field->options;
+    $field = transFields(get_defined_vars());
+    $val = Form::getValueAttribute($name);
+    $options = $field->options;
 @endphp
 <fieldset class="border p-2">
     <legend>{{ $field->label }}</legend>
-    @foreach($field->options as $k=>$label)
+    @foreach ($field->options as $k => $label)
         <div class="form-check">
-            <input class="form-check-input" name="{{ $name }}[]" value="{{$k}}" type="checkbox" {{\in_array($k,$val)?'checked':''}}  >
+            <input class="form-check-input" name="{{ $name }}[]" value="{{ $k }}" type="checkbox"
+                {{ \in_array($k, $val ?? []) ? 'checked' : '' }}>
             <label class="form-check-label">{{ $label }}</label>
         </div>
     @endforeach
 </fieldset>
-
