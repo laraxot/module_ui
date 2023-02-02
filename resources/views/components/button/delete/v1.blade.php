e1aa483ae15239ab11041f00809bb06ba5e08bfb
Retype GitHub Action <retype@object.net>
2023-02-02 11:45:36 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/views/components/button/delete/v1.blade.php
+++ resources/views/components/button/delete/v1.blade.php
@@ -1,4 +1,4 @@
 <a href="{{ $panel->url('destroy') }}" data-href="{{ $panel->url('destroy') }}" title="delete" data-toggle="tooltip"
-    class="btn btn-primary mb-2 btn-danger btn-confirm-delete">
-    <i class="far fa-trash-alt"></i>
+    {{ $attributes->merge($attrs['button']) }}>
+    <i {{ $attributes->merge($attrs['icon']) }}></i>
 </a>
--- resources/views/components/button/delete/v2.blade.php
+++
@@ -1,2 +0,0 @@
-<a href="{{ $panel->url('destroy') }}" class="btn btn-sm btn-square btn-neutral text-danger-hover"><i
-        class="bi bi-trash"></i></button>
