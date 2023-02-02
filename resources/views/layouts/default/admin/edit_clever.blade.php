aa2a04169b5057d1e57d4bbd19b9cd53e7d74884
Retype GitHub Action <retype@object.net>
2023-02-02 13:32:02 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

---
+++ resources/views/layouts/default/admin/edit_clever.blade.php
@@ -0,0 +1,27 @@
+@extends('adm_theme::layouts.app')
+@php
+    if (!is_object($row)) {
+        return '';
+    }
+    
+    $fields = $_panel->getFields('edit');
+@endphp
+
+@section('content')
+    <x-section type="crud">
+        <x-include-view view="topbar" />
+        {!! Form::bsOpenPanel($_panel, 'update') !!}
+        <div class="row">
+            @foreach ($fields as $field)
+                {!! Theme::inputHtml($field, $row) !!}
+
+                {{-- WIP
+                <x-input.field :field="$field" :row="$row" />
+                --}}
+            @endforeach
+        </div>
+        <button type="submit"
+            class="btn btn-sm bg-soft-success bg-opacity-20 bg-opacity-100-hover text-primary text-white-hover">Modifica</button>
+        {!! Form::close() !!}
+    </x-section>
+@endsection
