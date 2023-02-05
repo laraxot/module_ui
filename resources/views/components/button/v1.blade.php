2f0337ebe1d1ac7a9ccd5b1313078b648e559292
Retype GitHub Action <retype@object.net>
2023-02-05 09:18:31 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/views/components/button/v1.blade.php
+++ resources/views/components/button/v1.blade.php
@@ -3,7 +3,14 @@
 ])
 
 @if ($url != null)
-    <a href="{{ $url }}" {{ $attributes }}>{{ $slot }}</a>
+    {{-- <a href="{{ $url }}" {{ $attributes }}>{{ $slot }}</a> --}}
+    <a href="{{ $url }}" {{ $attributes->merge($attrs) }}>
+        @if (isset($attrs['icon']))
+            <i class="{{ $attrs['icon'] }}"></i>
+        @else
+            {{ $slot }}
+        @endif
+    </a>
 @else
     <button {{ $attributes->merge($attrs) }}>{{ $slot }}</button>
 @endif
