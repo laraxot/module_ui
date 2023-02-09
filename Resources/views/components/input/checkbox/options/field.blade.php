173b0c74cdf76d5a226adf8ea456dca9e688d30a
Malebestia <vair81@gmail.com>
2023-02-09 20:08:28 +0100

    up

--- Resources/views/components/input/checkbox/options/field.blade.php
+++ Resources/views/components/input/checkbox/options/field.blade.php
@@ -1,6 +1,7 @@
-<div class="form-check" wire:ignore> 
+<div class="form-check" wire:ignore>
     @foreach ($options as $key => $option)
-        <input type="checkbox" name="{{ $name }}[]" value="{{ $key }}" wire:model="form_data.{{ $name }}.{{ $key }}">
+        <input type="checkbox" name="{{ $name }}[]" value="{{ $key }}"
+            wire:model="form_data.{{ $name }}.{{ $key }}">
         <label for="{{ $attrs['name'] }}"> {{ $option }}</label><br>
     @endforeach
 </div>
