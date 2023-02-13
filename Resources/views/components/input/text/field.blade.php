af842f3839d274839fe69135ad5ea3e870fdb23a
marco76tv <marco.sottana@gmail.com>
2023-02-13 11:59:02 +0100

    up

--- Resources/views/components/input/text/field.blade.php
+++ Resources/views/components/input/text/field.blade.php
@@ -2,6 +2,7 @@
     <div class="input-label-absolute input-label-absolute-right">
         <div class="label-absolute"><i class="fa fa-search"></i></div>
 @endif
+{{-- {{ dddx($attributes->merge($attrs)) }} --}}
 <input type="text" {{ $attributes->merge($attrs) }} />
 @if (isset($icon))
     </div>
