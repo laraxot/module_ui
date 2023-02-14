f1a33bddb10f3c5a5390890c722c4f320f6d0052
Retype GitHub Action <retype@object.net>
2023-02-14 18:07:07 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/views/components/input/email/verified/field.blade.php
+++ resources/views/components/input/email/verified/field.blade.php
@@ -1,6 +1,1 @@
-@php
-    $val = Form::getValueAttribute($name);
-    $model = Form::getModel();
-@endphp
-
-<livewire:input.email.verified tpl="v1" :attrs="$attributes->merge($attrs)" :value="$val" />
+<livewire:input.email.verified tpl="v1" :attrs="$attrs" />
