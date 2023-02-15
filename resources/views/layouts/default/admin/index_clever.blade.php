74eeb46b118e05267e543132dceaafc2b58b2271
Retype GitHub Action <retype@object.net>
2023-02-15 16:01:31 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/views/layouts/default/admin/index_clever.blade.php
+++ resources/views/layouts/default/admin/index_clever.blade.php
@@ -2,13 +2,13 @@
 @section('content')
     {{--
     <x-topbar />
-    --}} 
+    --}}
     {!! $_panel->indexNav() !!}
-    <x-include-view view="inner_page" :vars="get_defined_vars()"/>
-    
+    <x-include-view view="inner_page" :vars="get_defined_vars()" />
+
     @component('ui::components.crud', get_defined_vars())
         @slot('content')
-            <x-include-view view="topbar" :vars="get_defined_vars()"/>
+            <x-include-view view="topbar" :vars="get_defined_vars()" />
             <!-- Table -->
             <div class="card">
                 <div class="card-header border-bottom d-flex align-items-center">
