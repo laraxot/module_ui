3ead545f8ee3b62457b3c457c8bd795b9ea76214
Malebestia <vair81@gmail.com>
2023-02-16 16:28:17 +0100

    up

--- View/Components/Badge.php
+++ View/Components/Badge.php
@@ -6,30 +6,35 @@ namespace Modules\UI\View\Components;
 
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\View\Component;
+use Modules\Cms\Actions\GetStyleClassByViewAction;
+use Modules\Cms\Actions\GetViewAction;
 
 class Badge extends Component {
     public string $tpl;
-
+    public string $view;
     public array $attrs = [];
 
     public function __construct(string $tpl = 'v1') {
         $this->tpl = $tpl;
-        $this->attrs['class'] = 'badge badge-primary';
-        if (inAdmin()) {
-            $class = config('adm_theme::styles.badge');
-        } else {
-            $class = config('pub_theme::styles.badge');
-        }
-        if (! is_null($class)) {
-            $this->attrs['class'] = config('pub_theme::styles.badge');
-        }
+        // $this->attrs['class'] = 'badge badge-primary';
+        // if (inAdmin()) {
+        //     $class = config('adm_theme::styles.badge');
+        // } else {
+        //     $class = config('pub_theme::styles.badge');
+        // }
+        // if (! is_null($class)) {
+        //     $this->attrs['class'] = config('pub_theme::styles.badge');
+        // }
+
+        $this->view = app(GetViewAction::class)->execute($this->tpl);
+        $this->attrs['class'] = app(GetStyleClassByViewAction::class)->execute($this->view);
     }
 
     public function render(): Renderable {
         /**
          * @phpstan-var view-string
          */
-        $view = 'ui::components.badge.'.$this->tpl;
+        $view = $this->view;
 
         $view_params = [];
 
