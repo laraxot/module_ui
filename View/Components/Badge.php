0d244682d7add7c5202ecc04a025f13e2d94a200
Malebestia <vair81@gmail.com>
2023-02-16 15:46:44 +0100

    up

--- View/Components/Badge.php
+++ View/Components/Badge.php
@@ -7,15 +7,13 @@ namespace Modules\UI\View\Components;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\View\Component;
 
-class Badge extends Component
-{
-    public string $type;
+class Badge extends Component {
+    public string $tpl;
 
     public array $attrs = [];
 
-    public function __construct(string $type = 'v1')
-    {
-        $this->type = $type;
+    public function __construct(string $tpl = 'v1') {
+        $this->tpl = $tpl;
         $this->attrs['class'] = 'badge badge-primary';
         if (inAdmin()) {
             $class = config('adm_theme::styles.badge');
@@ -27,12 +25,11 @@ class Badge extends Component
         }
     }
 
-    public function render(): Renderable
-    {
+    public function render(): Renderable {
         /**
          * @phpstan-var view-string
          */
-        $view = 'ui::components.badge.'.$this->type;
+        $view = 'ui::components.badge.'.$this->tpl;
 
         $view_params = [];
 
