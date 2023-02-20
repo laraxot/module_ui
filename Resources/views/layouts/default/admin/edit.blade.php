62f86485ccc45cac3459b111a09713dc7312138f
Malebestia <vair81@gmail.com>
2023-02-20 14:54:53 +0100

    up

--- Resources/views/layouts/default/admin/edit.blade.php
+++ Resources/views/layouts/default/admin/edit.blade.php
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
-{{-- @include($view_work . current_theme_name()) --}}
-
-@include($view_work . '_clever')
-=======
 @include($view_work . '_clever')
 {{--  @include($view_work . current_theme_name()) --}}
->>>>>>> 58d6e2f (up)
