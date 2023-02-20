2da1aa027971f6f61d7bf2b069477485d075a861
Retype GitHub Action <retype@object.net>
2023-02-20 11:22:49 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/views/layouts/default/admin/edit.blade.php
+++ resources/views/layouts/default/admin/edit.blade.php
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
 {{-- @include($view_work . current_theme_name()) --}}
 
 @include($view_work . '_clever')
+=======
+@include($view_work . '_clever')
+{{--  @include($view_work . current_theme_name()) --}}
+>>>>>>> 58d6e2f (up)
--- resources/views/layouts/default/admin/show.blade.php
+++ resources/views/layouts/default/admin/show.blade.php
@@ -1,1 +1,4 @@
+@include($view_work . '_clever')
+{{--
 @include($view_work . current_theme_name())
+--}}
