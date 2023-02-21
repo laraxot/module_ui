a4635899aafad15ff5ab01c9672a104fe0995898
Malebestia <vair81@gmail.com>
2023-02-21 19:23:45 +0100

    up

--- Actions/GetCollectiveViewByType.php
+++ Actions/GetCollectiveViewByType.php
@@ -11,10 +11,21 @@ class GetCollectiveViewByType {
 
     public function execute(string $type): string {
         // $json=__DIR__.'/_components.json';
+
+        // date.datetime.range
+        // bsDateDatetimeRange
+        if (count(explode('.', $type)) > 0) {
+            $tmp = '';
+            foreach (explode('.', $type) as $t) {
+                $tmp .= ucfirst($t);
+            }
+            $type = $tmp;
+        }
+
         $coll = app(GetCollectiveComponents::class)->execute();
         $res = collect($coll)->firstWhere('name', 'bs'.ucfirst($type));
         if (null == $res) {
-            dddx([$type, 'bs'.ucfirst($type), collect($coll)->firstWhere('name', 'bs'.ucfirst($type))]);
+            // dddx([$type, 'bs'.ucfirst($type), collect($coll)->firstWhere('name', 'bs'.ucfirst($type))]);
             throw new \Exception('['.__LINE__.']['.__FILE__.']');
         }
 
