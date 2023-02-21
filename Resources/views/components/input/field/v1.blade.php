9e6835b23c44b508ad02cef806d1b2caf54f263e
marco76tv <marco.sottana@gmail.com>
2023-02-21 13:02:41 +0100

    .

--- Resources/views/components/input/field/v1.blade.php
+++ Resources/views/components/input/field/v1.blade.php
@@ -1,15 +1,15 @@
 @props([
    
 ])
-
-{{-- <br />[attributes]: {{ $attributes }} ]
+{{--  
+<br />[attributes]: {{ $attributes }} ]
 <br />[div]: {{ $div_attrs }}]
 <br />[label]: {{ $label_attrs }} ]
-<br />[input]: {{ $input_attrs }} ] --}}
-
+<br />[input]: {{ $input_attrs }} ] 
+--}}
 <div {{ $div_attrs }}>
     <x-input.label :name="$field->name" :id="$field->name"   />
-    <x-input :name="$field->name" :type="$field->type" :class="$field->getInputClass()" wire:model="form_data.{{ $field->getNameDot() }}" :options="$field->options"/>
+    <x-input :name="$field->name" :type="$field->type" :value="$value" :class="$field->getInputClass()" :options="$field->options" :attributes="$field->attributes" />
     @error($field->name)
         <div class="alert alert-danger">{{ $message }}</div>
     @enderror
