88cc54b851e734acb2eff74debe1a99a05d43066
marco xot <marco.sottana@gmail.com>
2023-02-27 18:47:59 +0100

    up

--- View/Components/Card.php
+++ View/Components/Card.php
@@ -6,27 +6,27 @@ namespace Modules\UI\View\Components;
 
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\View\Component;
+use Modules\Cms\Actions\GetViewAction;
 
-class Card extends Component
-{
-    public ?string $type = 'card';
+class Card extends Component {
+    public string $tpl;// = 'card';
 
     public array $attrs = [];
 
-    public function __construct(?string $type = null)
+    public function __construct(string $tpl = 'card')
     {
-        $this->type = $type ?? 'card';
+        $this->tpl = $tpl;
     }
 
-    public function render(): Renderable
-    {
+    public function render(): Renderable {
         /**
          * @phpstan-var view-string
          */
-        $view = 'ui::components.card.'.$this->type;
+        //$view = 'ui::components.card.'.$this->type;
+        $view=app(GetViewAction::class)->execute($this->tpl);
 
         $view_params = [];
 
-        return view()->make($view, $view_params);
+        return view($view, $view_params);
     }
 }
