15c01dfe2495d92bf532d5c0c9651796f470e0b5
marco76tv <marco.sottana@gmail.com>
2023-02-27 19:18:43 +0100

    up

--- View/Components/Card.php
+++ View/Components/Card.php
@@ -9,12 +9,11 @@ use Illuminate\View\Component;
 use Modules\Cms\Actions\GetViewAction;
 
 class Card extends Component {
-    public string $tpl;// = 'card';
+    public string $tpl; // = 'card';
 
     public array $attrs = [];
 
-    public function __construct(string $tpl = 'card')
-    {
+    public function __construct(string $tpl = 'card') {
         $this->tpl = $tpl;
     }
 
@@ -22,8 +21,8 @@ class Card extends Component {
         /**
          * @phpstan-var view-string
          */
-        //$view = 'ui::components.card.'.$this->type;
-        $view=app(GetViewAction::class)->execute($this->tpl);
+        // $view = 'ui::components.card.'.$this->type;
+        $view = app(GetViewAction::class)->execute($this->tpl);
 
         $view_params = [];
 
