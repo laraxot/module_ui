a6dd7f60a6462aa0a97a819c8256537464a0f3e5
Retype GitHub Action <retype@object.net>
2023-02-28 09:57:46 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/views/components/input/field/v1.blade.php
+++ resources/views/components/input/field/v1.blade.php
@@ -1,6 +1,4 @@
-@props([
-   
-])
+@props([])
 {{--  
 <br />[attributes]: {{ $attributes }} ]
 <br />[div]: {{ $div_attrs }}]
@@ -8,10 +6,11 @@
 <br />[input]: {{ $input_attrs }} ] 
 --}}
 <div {{ $div_attrs }}>
-    <x-input.label :name="$field->name" :id="$field->name"   />
-    <x-input :name="$field->name" :type="$field->type" :value="$value" :class="$field->getInputClass()" :options="$field->options" :attributes="$field->attributes" />
+    <x-input.label :name="$field->name" :id="$field->name" />
+    <x-input :name="$field->name" :type="$field->type" :value="$value" :class="$field->getInputClass()" :options="$field->options"
+        :attributes="$field->attributes" />
     @error($field->name)
         <div class="alert alert-danger">{{ $message }}</div>
     @enderror
-    
+
 </div>
