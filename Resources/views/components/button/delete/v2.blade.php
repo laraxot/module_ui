78bdb84cfff212503e24623f3e1d465f71360593
marco76tv <marco.sottana@gmail.com>
2023-03-03 10:08:10 +0100

    up

--- Resources/views/components/button/delete/v2.blade.php
+++ Resources/views/components/button/delete/v2.blade.php
@@ -1,4 +1,5 @@
-<a href="#" onclick="Livewire.emit('modal.open', 'modal.panel.destroy', {'model_type':'{{str_replace("\\","\\\\",get_class($panel->row))}}','model_id': '{{$panel->row->id}}'})" title="delete" data-toggle="tooltip"
-    {{ $attributes->merge($attrs['button']) }}>
-    <i {{ $attributes->merge($attrs['icon']) }}></i>
+<a href="#"
+    onclick="Livewire.emit('modal.open', 'modal.panel.destroy', {'model_type':'{{ str_replace('\\', '\\\\', get_class($panel->row)) }}','model_id': '{{ $panel->row->id }}'})"
+    title="delete" data-toggle="tooltip" {{ $attributes->merge($attrs) }}>
+    {!! $icon !!}
 </a>
