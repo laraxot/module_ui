b1f820f8904a83c025e2ab071621715fa29b855c
Retype GitHub Action <retype@object.net>
2023-03-06 13:42:15 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/views/livewire/input/sms/verified/v1.blade.php
+++ resources/views/livewire/input/sms/verified/v1.blade.php
@@ -1,16 +1,23 @@
 <div>
 
-    @if ($is_sent === true)
-        <h2>Write the code that you received via sms</h2>
+    @if ($step === 1)
+        <h2>Validated Sms Addresses</h2>
         <div class="input-group mb-3">
-            <input wire:model.lazy="form_data.token" type="text" class="form-control" placeholder="Verification Code"
-                aria-label="Verification Code" aria-describedby="basic-addon2">
+            <select wire:model.lazy="{{ $attrs['wire:model.lazy'] }}" wire:change="updateFormData"
+                name="{{ $attrs['name'] }}" class="{{ $attrs['class'] }}" aria-label="Default select example">
+                <option selected>Select Mobile Number</option>
+                @foreach ($my_validated_sms_addresses as $sms)
+                    <option value="{{ $sms->value }}">{{ $sms->value }}</option>
+                @endforeach
+            </select>
             <div class="input-group-append">
-                <button wire:click="verify_code()" class="btn btn-success" type="button">Verify</button>
+                <button wire:click="add()" class="btn btn-info" type="button">Add</button>
             </div>
         </div>
-    @else
-        <div id="add_sms_form" style="display: none">
+    @endif
+
+    @if ($step === 2)
+        <div id="add_sms_form">
             <h2>Add Mobile Number</h2>
             <div class="input-group mb-3">
                 <input wire:model.lazy="form_data.add_mobile" type="text" class="form-control"
@@ -22,19 +29,16 @@
         </div>
     @endif
 
-    <h2>Validated Sms Addresses</h2>
-    <div class="input-group mb-3">
-        <select wire:model.lazy="{{ $attrs['wire:model.lazy'] }}" wire:change="updateFormData"
-            name="{{ $attrs['name'] }}" class="{{ $attrs['class'] }}" aria-label="Default select example">
-            <option selected>Select Mobile Number</option>
-            @foreach ($my_validated_sms_addresses as $sms)
-                <option value="{{ $sms->value }}">{{ $sms->value }}</option>
-            @endforeach
-        </select>
-        <div class="input-group-append">
-            <button onclick=" $('#add_sms_form').toggle()" class="btn btn-info" type="button">Add</button>
+    @if ($step === 3)
+        <h2>Write the code that you received via sms</h2>
+        <div class="input-group mb-3">
+            <input wire:model.lazy="form_data.token" type="text" class="form-control" placeholder="Verification Code"
+                aria-label="Verification Code" aria-describedby="basic-addon2">
+            <div class="input-group-append">
+                <button wire:click="verify_code()" class="btn btn-success" type="button">Verify</button>
+            </div>
         </div>
-    </div>
+    @endif
 
     <x-flash-message />
 </div>
