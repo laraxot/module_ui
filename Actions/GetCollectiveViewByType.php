58528cb7646328754ecb6eb763c05fc85853c00b
marco76tv <marco.sottana@gmail.com>
2023-03-10 11:14:41 +0100

    up

--- Actions/GetCollectiveViewByType.php
+++ Actions/GetCollectiveViewByType.php
@@ -6,10 +6,12 @@ namespace Modules\UI\Actions;
 
 use Spatie\QueueableAction\QueueableAction;
 
-class GetCollectiveViewByType {
+class GetCollectiveViewByType
+{
     use QueueableAction;
 
-    public function execute(string $type): string {
+    public function execute(string $type): string
+    {
         // $json=__DIR__.'/_components.json';
 
         // date.datetime.range
@@ -25,11 +27,11 @@ class GetCollectiveViewByType {
 
         $coll = app(GetCollectiveComponents::class)->execute();
 
-        $res = collect($coll)->firstWhere('name', 'bs'.ucfirst($type));
+        $res = collect($coll)->firstWhere('name', 'bs' . ucfirst($type));
 
         if (null == $res) {
             // dddx([$type, 'bs'.ucfirst($type), collect($coll)->firstWhere('name', 'bs'.ucfirst($type))]);
-            throw new \Exception('['.$type.']['.__LINE__.']['.__FILE__.']');
+            throw new \Exception('[' . $type . '][' . __LINE__ . '][' . __FILE__ . ']');
         }
 
         return $res->view;
