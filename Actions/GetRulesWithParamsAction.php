f20387511bf5df521d2f252939916083114a0f3f
marco76tv <marco.sottana@gmail.com>
2023-03-10 13:17:57 +0100

    up

--- Actions/GetRulesWithParamsAction.php
+++ Actions/GetRulesWithParamsAction.php
@@ -22,7 +22,7 @@ class GetRulesWithParamsAction
     {
         $validatorClass = new ReflectionClass(ValidatesAttributes::class);
 
-        return collect($validatorClass->getMethods(ReflectionMethod::IS_PUBLIC))
+        $r = collect($validatorClass->getMethods(ReflectionMethod::IS_PUBLIC))
             ->filter(function ($method) {
                 return Str::startsWith($method->name, 'validate');
             })
@@ -34,38 +34,100 @@ class GetRulesWithParamsAction
 
                 $params = $this->getParamsType($method_name);
 
-                $start = strpos($method->getDocComment(), 'Validate');
-                $end = strpos($method->getDocComment(), '@');
+                if (null != $params) {
 
-                $comment = '';
-                if (is_int($start)) {
-                    $comment = substr($method->getDocComment(), $start, $end - $start);
-                    $comment = preg_replace('/\s\s+/', ' ', $comment);
-                    $comment = str_replace('* ', '', $comment);
-                    $comment = trim($comment);
+                    $start = strpos($method->getDocComment(), 'Validate');
+                    $end = strpos($method->getDocComment(), '@');
+
+                    $comment = '';
+                    if (is_int($start)) {
+                        $comment = substr($method->getDocComment(), $start, $end - $start);
+                        $comment = preg_replace('/\s\s+/', ' ', $comment);
+                        $comment = str_replace('* ', '', $comment);
+                        $comment = trim($comment);
+                    }
+
+                    return [
+                        'name' => $method_name,
+                        'comment' => $comment,
+                        'params' => $params
+                    ];
                 }
-
-                return [
-                    'name' => $method_name,
-                    'comment' => $comment,
-                    'params' => $params
-                ];
+            })->filter(function ($v) {
+                return $v !== null;
             })->toArray();
+
+        //dd($r);
+
+        return $r;
     }
 
-    public function getParamsType(string $method_name): array
+    /**
+     * @param string $method_name
+     * @return array|null
+     */
+    public function getParamsType(string $method_name)
     {
         $parameters = [
+            'accepted' => [''],
+            'active_url' => [''],
+            'after' => ['date' => 'text'],
+            'after_or_equal' => ['date' => 'text'],
+            'before' => ['date' => 'text'],
+            'before_or_equal' => ['date' => 'text'],
+            'boolean' => [''],
+            'date' => [''],
+            'date_equals' => ['date' => 'text'],
+            'decimal' => [
+                'min' => 'number',
+                'max' => 'number'
+            ],
+            'declined' => [''],
+            'different' => ['field' => 'text'],
+            'digits' => ['value' => 'number'],
             'digits_between' => [
                 'min' => 'number',
                 'max' => 'number'
             ],
+            'email' => [''],
+            'filled' => [''],
+            'gt' => ['field' => 'text'],
+            'gte' => ['field' => 'text'],
+            'integer' => [''],
+            'ip' => [''],
+            'json' => [''],
+            'lt' => ['field' => 'text'],
+            'lte' => ['field' => 'text'],
+            'lowercase' => [''],
+            'mac_address' => [''],
+            'max' => ['value' => 'number'],
+            'max_digits' => ['value' => 'number'],
+            'min' => ['value' => 'number'],
+            'min_digits' => ['value' => 'number'],
+            'multiple_of' => ['value' => 'number'],
+            'not_regex' => ['pattern' => 'text'],
+            'null' => [''],
+            'numeric' => [''],
+            'present' => [''],
+            'regex' => ['pattern' => 'text'],
+            'required' => [''],
+            'same' => ['field' => 'text'],
+            'size' => ['value' => 'number'],
+            'starts_with' => ['values' => 'text'],
+            'string' => [''],
+            'timezone' => [''],
+            'unique' => ['table' => 'text', 'column' => 'text'],
+            'uppercase' => [''],
+            'url' => [''],
+            'uuid' => [''],
+
         ];
 
         if (isset($parameters[$method_name])) {
             return $parameters[$method_name];
         }
 
-        return ['json' => 'json'];
+        return null;
+        //return ['json' => 'json'];
     }
 }
