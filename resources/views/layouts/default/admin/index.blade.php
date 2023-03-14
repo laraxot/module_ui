c3a6e311004b4a2d83446ce15de04376693a7619
Retype GitHub Action <retype@object.net>
2023-03-14 11:53:59 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/views/layouts/default/admin/index.blade.php
+++ resources/views/layouts/default/admin/index.blade.php
@@ -84,15 +84,6 @@
                 <!-- Filters -->
                 <div class="d-flex flex-column flex-md-row gap-3 justify-content-between">
                     <div class="d-flex gap-3">
-                        <!-- Search -->
-                        {{-- <div class="input-group input-group-sm input-group-inline">
-                            <span class="input-group-text pe-2">
-                                <i class="bi bi-search"></i>
-                            </span>
-
-                            <input type="text" class="form-control" placeholder="Search" aria-label="Search"
-                                name="q">
-                        </div> --}}
                         <x-form.search />
                         <!-- Filters -->
                         {{-- <div>
@@ -106,80 +97,21 @@
                     </div>
                     <div class="btn-group">
                         @foreach ($_panel->getActions('container') as $action)
-                            {{-- <x-button.action :action="$action"></x-button.action> --}}
+                            <x-button.action :action="$action"></x-button.action>
+                            {{--
                             <x-button.link :link="$action->getLinkData()" />
+                            --}}
                         @endforeach
-                        {{-- <a href="#" class="btn btn-sm btn-neutral text-primary" aria-current="page">View
-                            all</a>
-                        <a href="#" class="btn btn-sm btn-neutral">Private</a>
-                        <a href="#" class="btn btn-sm btn-neutral">Shared files</a> --}}
                     </div>
                 </div>
                 <!-- Table -->
                 <div class="card">
                     <div class="card-header border-bottom d-flex align-items-center">
                         <h5 class="me-auto">{{ $_panel_name }}</h5>
-                        {{-- <div class="dropdown">
-                            <a class="text-muted" href="#" role="button" data-bs-toggle="dropdown"
-                                aria-haspopup="true" aria-expanded="false">
-                                <i class="bi bi-three-dots-vertical"></i>
-                            </a>
-                            <div class="dropdown-menu">
-                                <a href="#!" class="dropdown-item">
-                                    Action
-                                </a>
-                                <a href="#!" class="dropdown-item">
-                                    Another action
-                                </a>
-                                <a href="#!" class="dropdown-item">
-                                    Something else here
-                                </a>
-                            </div>
-                        </div> --}}
                     </div>
 
                     <x-panel.crud :panel="$_panel" :rows="$rows" />
 
-                    {{-- @php
-                        $fields = $_panel->getFields('index');
-                    @endphp
-                    <div class="table-responsive">
-                        <table class="table table-hover table-nowrap">
-                            @foreach ($rows as $row)
-                                @php
-                                    $row_panel = $_panel->newPanel($row);
-                                @endphp
-                                @if ($loop->first)
-                                    <thead class="table-light">
-                                        <tr>
-                                            @foreach ($fields as $field)
-                                                <th scope="col">{{ $field->name }}</th>
-                                            @endforeach
-                                            <th></th>
-                                        </tr>
-                                    </thead>
-                                @endif
-                                <tbody>
-
-                                    <tr>
-                                        @foreach ($fields as $field)
-                                            <td>
-                                                <x-input.freeze :field="$field" :row="$row" />
-                                            </td>
-                                        @endforeach
-
-                                        <td class="text-end">
-                                            <x-panel.buttons.crud :panel="$row_panel" />
-                                        </td>
-                                    </tr>
-
-                                </tbody>
-                            @endforeach
-                        </table>
-                    </div>
-                    <div class="card-footer border-0 py-5">
-                        <span class="text-muted text-sm">Showing 10 items out of 250 results found</span>
-                    </div> --}}
                 </div>
             </div>
         </div>
