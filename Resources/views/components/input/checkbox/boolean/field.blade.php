c5a27864d8f639ca8de25cfe0ccee469780d292c
marco76tv <marco.sottana@gmail.com>
2023-03-17 18:04:09 +0100

    up

--- Resources/views/components/input/checkbox/boolean/field.blade.php
+++ Resources/views/components/input/checkbox/boolean/field.blade.php
@@ -2,8 +2,16 @@
     'label'=>null,
     'id'=>null,
 ])
+{{--
 <div class="form-check form-switch">
   <input type="text" value="0" wire:model="form_data.{{ $name }}.{{ $id }}">
   <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" value="1" wire:model="form_data.{{ $name }}.{{ $id }}">
   <label class="form-check-label" for="flexSwitchCheckDefault">Acconsento</label>
 </div>
+--}}
+    <input type="hidden" name="{{ $name }}" id="{{ $name }}" value="0"
+        {{ old($name) ? '' : 'checked' }}>
+    <input type="checkbox" name="{{ $name }}" id="{{ $name }}" value="1" wire:model="form_data.{{ $name }}"
+        {{ old($name) ? 'checked' : '' }}>
+    {{ $label }}
+
