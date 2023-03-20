675d5742b80be4fd43f8006f8ce1b9f88fc945bc
marco76tv <marco.sottana@gmail.com>
2023-03-20 18:31:39 +0100

    up

--- View/Components/Input/Freeze.php
+++ View/Components/Input/Freeze.php
@@ -47,7 +47,8 @@ class Freeze extends Component
             */
             $this->value = $this->row->{$field->name} ?? Arr::get($row, $field->getNameDot());
         } catch (\Exception $e) {
-            dddx(['field' => $this->field, 'row' => $this->row, 'exception' => $e]);
+            //altrimenti dà errore "Undefined array key "Modules\PFed\Models\States\CompanyReport\Accepted"" ma non è una relazione
+            //dddx(['field' => $this->field, 'row' => $this->row, 'exception' => $e]);
         }
         /*
         if (is_countable($field->options) && count($field->options) > 0) {
