6dd6cc964948d9401ac71daa2b64a8ed013d12a3
Retype GitHub Action <retype@object.net>
2023-03-21 09:33:55 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/views/components/layout/wizard/v1.blade.php
+++ resources/views/components/layout/wizard/v1.blade.php
@@ -6,10 +6,23 @@
 <div>
     <div class="mb-5">
         <ul class="nav nav-tabs overflow-x border-0">
+            @php
+                $hasPrevious = false;
+                $hasNext = false;
+            @endphp
             @foreach ($steps as $step)
+                @php
+                    if ($step->isPrevious()) {
+                        $hasPrevious = true;
+                    }
+                    if ($step->isNext()) {
+                        $hasNext = true;
+                    }
+                @endphp
                 <li class="nav-item">
-                    <span href="#"
-                        class="nav-link {{ $step->isCurrent() ? 'active' : '' }}">{{ $step->label }}</span>
+                    <span href="#" class="nav-link {{ $step->isCurrent() ? 'active' : '' }}">
+                        {{ $step->label }}
+                    </span>
                 </li>
             @endforeach
         </ul>
@@ -18,18 +31,35 @@
 
     <x-flash-message />
 
+
     <nav aria-label="Page navigation example">
         <ul class="pagination">
-            <li class="page-item {{ $is_first ? 'disabled' : '' }}">
-                <a class="page-link" href="#" aria-label="Previous" wire:click="previousStep">
-                    <span aria-hidden="true">&laquo;</span>
-                </a>
-            </li>
-            <li class="page-item {{ $is_last ? 'disabled' : '' }}">
-                <a class="page-link" href="#" aria-label="Next" wire:click="goNextStep">
-                    <span aria-hidden="true">&raquo;</span>
-                </a>
-            </li>
+            @if ($hasPrevious)
+                <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Previous" wire:click="previousStep">
+                        <span aria-hidden="true">&laquo;</span>
+                    </a>
+                </li>
+            @else
+                <li class="page-item disabled">
+                    <a class="page-link" href="#" aria-label="Previous" wire:click="previousStep">
+                        <span aria-hidden="true">&nbsp;</span>
+                    </a>
+                </li>
+            @endif
+            @if ($hasNext)
+                <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Next" wire:click="goNextStep">
+                        <span aria-hidden="true">&raquo;</span>
+                    </a>
+                </li>
+            @else
+                <li class="page-item disabled">
+                    <a class="page-link" href="#" aria-label="Next" wire:click="goNextStep">
+                        <span aria-hidden="true">&nbsp;</span>
+                    </a>
+                </li>
+            @endif
         </ul>
     </nav>
 </div>
