b94587433913e047e119bec1b7576bf4c90c6dc4
Retype GitHub Action <retype@object.net>
2023-03-22 09:56:56 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/views/components/std/accordion/container.blade.php
+++ resources/views/components/std/accordion/container.blade.php
@@ -1,3 +1,12 @@
 <div {{ $attributes->merge($attrs) }} id="accordionExample">
-    <x-std tpl="accordion.item"></x-std>
+    <x-std tpl="accordion.item">
+        <x-slot name="id">{{ $id }}</x-slot>
+        <x-slot name="header">
+            {{ $header }}
+        </x-slot>
+
+        <x-slot name="body">
+            {{ $body }}
+        </x-slot>
+    </x-std>
 </div>
