3c5ca67c14a8ab94193c1f02e128f1e5e184780a
Retype GitHub Action <retype@object.net>
2023-03-27 09:50:47 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/views/components/input/cell/field.blade.php
+++ resources/views/components/input/cell/field.blade.php
@@ -1,4 +1,10 @@
-@foreach ($field->fields as $v)
-    <label><strong>{{ $v->getLabel() }}</strong>: </label>
-    <x-input.freeze :field="$v" :row="$row" /><br><br>
-@endforeach
+<div class="card" style="width: 18rem;">
+  <ul class="list-group list-group-flush">
+    @foreach ($field->fields as $v)
+        <li class="list-group-item">
+            <strong>{{ $v->getLabel() }}</strong>: 
+            <x-input.freeze :field="$v" :row="$row" />
+        </li>
+    @endforeach
+  </ul>
+</div>
