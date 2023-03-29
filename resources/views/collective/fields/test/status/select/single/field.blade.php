d5f69567340c146b794dc9d8d173e8c77fabbff6
Retype GitHub Action <retype@object.net>
2023-03-29 11:19:18 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/views/collective/fields/test/status/select/single/field.blade.php
+++ resources/views/collective/fields/test/status/select/single/field.blade.php
@@ -1,7 +1,3 @@
-@php
-
-//dddx(get_defined_vars());
-@endphp
 <div class="form-group col-sm-12">
     <livewire:input.status.select.single :model="$_panel->row" :options="$_panel->getStatusesList()" />
 </div>
