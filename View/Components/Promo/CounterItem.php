d1aacf3d65bfc4b8b8e4babd1a1d399aedcde07d
Malebestia <vair81@gmail.com>
2023-04-05 16:10:51 +0200

    up

---
+++ View/Components/Promo/CounterItem.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\UI\View\Components\Promo;
+
+use Illuminate\View\Component;
+
+/**
+ * Undocumented class.
+ */
+class CounterItem extends Component {
+    public int $counter;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct(int $counter) {
+        $this->counter = $counter;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|string
+     */
+    public function render() {
+        $view = app(GetViewAction::class)->execute();
+        $view_params = [
+            'view'=>$view
+        ];
+        return view($view,$view_params );
+    }
+}
