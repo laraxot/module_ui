59afe44c64b3f540407e734801bd7d7af4cab048
Retype GitHub Action <retype@object.net>
2023-04-07 10:42:24 +0000

    Refreshes Retype-generated documentation.
    Process triggered by Malebestia.

--- resources/views/components/std/accordion/item.blade.php
+++ resources/views/components/std/accordion/item.blade.php
@@ -1,11 +1,11 @@
 <div {{ $attributes->merge($attrs) }}>
-    <h2 class="accordion-header" id="heading{{$id}}">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{$id}}"
-            aria-expanded="true" aria-controls="collapse{{$id}}">
+    <h2 class="accordion-header" id="heading{{ $id }}">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+            data-bs-target="#collapse{{ $id }}" aria-expanded="true" aria-controls="collapse{{ $id }}">
             {{ $header }}
         </button>
     </h2>
-    <div id="collapse{{$id}}" class="accordion-collapse collapse" aria-labelledby="heading{{$id}}"
+    <div id="collapse{{ $id }}" class="accordion-collapse collapse" aria-labelledby="heading{{ $id }}"
         data-bs-parent="#accordionExample">
         <div class="accordion-body">
             {{ $body }}
