ca99ea72725ffb19f74353e7b798c2a5b53ecc85
marco76tv <marco.sottana@gmail.com>
2023-05-02 13:23:55 +0200

    up

--- docs/navigation.php
+++ docs/navigation.php
@@ -3,21 +3,6 @@
 declare(strict_types=1);
 
 return [
-    // 'Getting Started' => [
-    //     'url' => 'docs/getting-started',
-    //     'children' => [
-    //         'Customizing Your Site' => 'docs/customizing-your-site',
-    //         'Navigation' => 'docs/navigation',
-    //         'Algolia DocSearch' => 'docs/algolia-docsearch',
-    //         'Custom 404 Page' => 'docs/custom-404-page',
-    //         'Other Page' => 'docs/other-page',
-    //         'Other Page 2' => 'docs/other-page-2',
-    //     ],
-    // ],
-    // 'Other Section' => [
-    //     'url' => 'docs/other-section',
-    // ],
-    // 'Jigsaw Docs' => 'https://jigsaw.tighten.co/docs/installation',
     'Introduction' => [
         'url' => 'docs/introduction',
     ],
@@ -176,6 +161,12 @@ return [
                     'Builder' => 'docs/livewire_components/menu/builder',
                 ],
             ],
+        ],
+    ],
+    'links' => [
+        'url' => '#',
+        'children' => [
+            'blade components' => 'docs/links/blade_components',
         ],
     ],
 ];
---
+++ docs/source/docs/links/blade_components.md
@@ -0,0 +1,11 @@
+---
+title: links blade components
+description: 
+extends: _layouts.documentation
+section: content
+---
+
+# links blade components
+
+...
+
