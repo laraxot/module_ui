a3b8fe2d566aa60471c95330678b5b442acfb74b
Retype GitHub Action <retype@object.net>
2023-05-05 08:54:44 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/views/collective/fields/float/field.blade.php
+++ resources/views/collective/fields/float/field.blade.php
@@ -1,12 +1,22 @@
 @php
     $field = transFields(get_defined_vars());
+<<<<<<< HEAD
+    $field->attributes = array_merge($field->attributes, ['step' => '0.01']);
+    
+=======
     $field->attributes['step'] = '0.01';
+>>>>>>> e723df785962a32cfea807fe3cfe8354a0d087ca
 @endphp
 @component($blade_component, get_defined_vars())
     @slot('label')
-        {{ Form::label($name, $field->label, ['class' => 'control-label form-label']) }}
+        {{ Form::label($name, $field->label, ['class' => 'control-label']) }}
     @endslot
     @slot('input')
-        {{ Form::number($name, $value, $field->attributes) }}
+        <div class="input-group">
+            {{ Form::number($name, $value, $field->attributes) }}
+            <span class="input-group-addon">
+                <span {{-- class="glyphicon glyphicon-calendar" --}}>.xx</span>
+            </span>
+        </div>
     @endslot
 @endcomponent
