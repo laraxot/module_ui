cd45541fe3990a1c923b58180545216f8465999b
marco76tv <marco.sottana@gmail.com>
2023-05-12 17:27:55 +0200

    up

--- docs/config.php
+++ docs/config.php
@@ -1,25 +1,29 @@
 <?php
 
-declare(strict_types=1);
-
 use Illuminate\Support\Str;
+
+$moduleName = 'UI';
 
 return [
     'baseUrl' => '',
     'production' => false,
-    'siteName' => 'Modulo Theme',
-    'siteDescription' => 'Beautiful docs powered by Jigsaw',
-    'lang' => 'it',
+    'siteName' => 'Modulo '.$moduleName,
+    'siteDescription' => 'Modulo '.$moduleName,
+    //'lang' => 'it',
 
     'collections' => [
         'posts' => [
             'path' => function ($page) {
-                return $page->lang.'/posts/'.Str::slug($page->getFilename());
+                //return $page->lang.'/posts/'.Str::slug($page->getFilename());
+                //return 'posts/' . ($page->featured ? 'featured/' : '') . Str::slug($page->getFilename());
+
+                return 'posts/'.Str::slug($page->getFilename());
             },
         ],
         'docs' => [
             'path' => function ($page) {
-                return $page->lang.'/docs/'.Str::slug($page->getFilename());
+                //return $page->lang.'/docs/'.Str::slug($page->getFilename());
+                return 'docs/'.Str::slug($page->getFilename());
             },
         ],
     ],
@@ -35,25 +39,22 @@ return [
     'isActive' => function ($page, $path) {
         return Str::endsWith(trimPath($page->getPath()), trimPath($path));
     },
-    'isItemActive' => function ($page, $item) {
-        return Str::endsWith(trimPath($page->getPath()), trimPath($item->getPath()));
-    },
     'isActiveParent' => function ($page, $menuItem) {
         if (is_object($menuItem) && $menuItem->children) {
             return $menuItem->children->contains(function ($child) use ($page) {
                 return trimPath($page->getPath()) == trimPath($child);
             });
         }
+    },/*
+    'url' => function ($page, $path) {
+        return Str::startsWith($path, 'http') ? $path : '/' . trimPath($path);
     },
+    */
     'url' => function ($page, $path) {
         if (Str::startsWith($path, 'http')) {
             return $path;
         }
-        // return Str::startsWith($path, 'http') ? $path : '/' . trimPath($path);
-        return url('/'.$page->lang.'/'.trimPath($path));
-    },
-
-    'children' => function ($page, $docs) {
-        return $docs->where('parent_id', $page->id);
+         //return url('/'.$page->lang.'/'.trimPath($path));
+        return url('/'.trimPath($path));
     },
 ];
