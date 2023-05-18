16f6d176b70220e1f5e101fc38569305d37ea3ce
marco76tv <marco.sottana@gmail.com>
2023-05-18 17:58:25 +0200

    up

--- Providers/UIServiceProvider.php
+++ Providers/UIServiceProvider.php
@@ -21,7 +21,8 @@ use Modules\Xot\Services\FileService;
 /**
  * Class UIServiceProvider.
  */
-class UIServiceProvider extends XotBaseServiceProvider {
+class UIServiceProvider extends XotBaseServiceProvider
+{
     protected string $module_dir = __DIR__;
 
     protected string $module_ns = __NAMESPACE__;
@@ -33,14 +34,16 @@ class UIServiceProvider extends XotBaseServiceProvider {
     /**
      * Undocumented function.
      */
-    public function getXot(): XotData {
+    public function getXot(): XotData
+    {
         return $this->xot;
     }
 
     /**
      * Undocumented function.
      */
-    public function bootCallback(): void {
+    public function bootCallback(): void
+    {
         if ($this->app->runningInConsole()) {
             /*
             $this->publishes([
@@ -50,7 +53,7 @@ class UIServiceProvider extends XotBaseServiceProvider {
             $this->mergeConfigFrom(__DIR__.'/../Config/xra.php', 'xra');
         }
 
-        $this->xot = XotData::from(config('xra'));
+        $this->xot = XotData::make();
 
         $this->commands(
             [
@@ -58,14 +61,15 @@ class UIServiceProvider extends XotBaseServiceProvider {
             ]
         );
 
-        //BladeService::registerComponents($this->module_dir.'/../View/Components', 'Modules\\UI');
+        // BladeService::registerComponents($this->module_dir.'/../View/Components', 'Modules\\UI');
 
         $this->registerCollective();
 
         Paginator::useBootstrap();
     }
 
-    public function registerCollective(): void {
+    public function registerCollective(): void
+    {
         app(RegisterCollectiveComponents::class)->execute(
             $this->module_dir.'/../Resources/views/collective/fields',
             $this->module_name.'::'
@@ -79,7 +83,8 @@ class UIServiceProvider extends XotBaseServiceProvider {
      *
      * @return void
      */
-    public function bootThemeProvider(string $theme_type) {
+    public function bootThemeProvider(string $theme_type)
+    {
         // $xot = $this->getXot();
 
         $theme = $this->xot->{$theme_type};
@@ -107,7 +112,8 @@ class UIServiceProvider extends XotBaseServiceProvider {
      *
      * @return void
      */
-    public function registerNamespaces(string $theme_type) {
+    public function registerNamespaces(string $theme_type)
+    {
         $theme = $this->xot->{$theme_type};
 
         $resource_path = 'Themes/'.$theme.'/Resources';
@@ -119,7 +125,8 @@ class UIServiceProvider extends XotBaseServiceProvider {
         $this->loadTranslationsFrom($lang_dir, $theme_type);
     }
 
-    public function registerThemeConfig(string $theme_type): void {
+    public function registerThemeConfig(string $theme_type): void
+    {
         $theme = $this->xot->{$theme_type};
 
         $config_path = base_path('Themes/'.$theme.'/Config');
@@ -138,7 +145,8 @@ class UIServiceProvider extends XotBaseServiceProvider {
         }
     }
 
-    public function registerCallback(): void {
+    public function registerCallback(): void
+    {
         $loader = AliasLoader::getInstance();
         $loader->alias('Theme', 'Modules\UI\Services\ThemeService');
     }
--- View/Components/Button.php
+++ View/Components/Button.php
@@ -4,54 +4,52 @@ declare(strict_types=1);
 
 namespace Modules\UI\View\Components;
 
+use Illuminate\Contracts\Support\Renderable;
 use Illuminate\View\Component;
-use Modules\Xot\Datas\XotData;
+use Modules\Cms\Actions\GetStyleClassByViewAction;
 use Modules\Cms\Actions\GetViewAction;
 use Modules\Cms\Services\PanelService;
-use Illuminate\Contracts\Support\Renderable;
-use Modules\Cms\Actions\GetStyleClassByViewAction;
+use Modules\Xot\Datas\XotData;
 
-class Button extends Component {
+class Button extends Component
+{
     public string $tpl;
     public string $type;
 
-    public array $attrs=[];
+    public array $attrs = [];
     public ?string $url = '#';
     public string $view;
 
     public string $tradKey;
-    
 
     /**
      * Create the component instance.
      *
      * @return void
      */
-    public function __construct(string $tpl = 'v1', string $type = 'button', ?string $title=null,array $attrs = []) {
-         //dddx(get_class_methods($this));
-         //dddx($this->resolve());
-         $xot=XotData::from(config('xra'));
+    public function __construct(string $tpl = 'v1', string $type = 'button', ?string $title = null, array $attrs = [])
+    {
+        // dddx(get_class_methods($this));
+        // dddx($this->resolve());
+        $xot = XotData::make();
         $this->tpl = $tpl;
         $this->type = $type;
-        
 
         $this->attrs = $attrs;
 
         $this->url = isset($attrs['url']) ? $attrs['url'] : null;
 
-        //$this->view = app(GetViewAction::class)->execute($this->type.'.'.$this->tpl);
+        // $this->view = app(GetViewAction::class)->execute($this->type.'.'.$this->tpl);
         $this->view = app(GetViewAction::class)->execute($this->tpl);
 
-        
-
-        //$this->attrs['class'] = app(GetStyleClassByViewAction::class)->execute($this->view);
+        // $this->attrs['class'] = app(GetStyleClassByViewAction::class)->execute($this->view);
         $this->attrs['class'] = app(GetStyleClassByViewAction::class)->execute($this->view.'.'.$this->type);
         $this->attrs['type'] = $type;
         // dddx($this->attrs['class']);
         $this->attrs['icon'] = isset($attrs['icon']) ? $attrs['icon'] : null;
         // $icon_class = inAdmin() ? 'adm_theme::styles.button.icon.'.$type : 'pub_theme::styles.button.icon.'.$type;
         // $this->attrs['icon'] = config($icon_class, 'fas fa-angle-double-left');
-        
+
         /*
         $this->tradKey = 'pub_theme::txt';
         if (class_exists(PanelService::class)) {
@@ -61,22 +59,21 @@ class Button extends Component {
             }
         }
         */
-        
-        //$this->tradKey = (isAdmin()?'adm_theme':'pub_theme').'::button';
+
+        // $this->tradKey = (isAdmin()?'adm_theme':'pub_theme').'::button';
         $this->tradKey = strtolower($xot->main_module).'::button';
-        
 
-        if($title!=null){
-            $title=trans($this->tradKey.'.'.$title);
-            $this->attrs['title']=$title;
+        if (null != $title) {
+            $title = trans($this->tradKey.'.'.$title);
+            $this->attrs['title'] = $title;
         }
-
     }
 
     /**
      * Undocumented function.
      */
-    public function render(): Renderable {
+    public function render(): Renderable
+    {
         /**
          * @phpstan-var view-string
          */
