9433eea30a5e6d53f8a5ac157d8b7d135684e7e6
Retype GitHub Action <retype@object.net>
2023-05-26 13:45:06 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- resources/views/components/input/checkbox/options/consents/field.blade.php
+++ resources/views/components/input/checkbox/options/consents/field.blade.php
@@ -1,12 +1,15 @@
 <div class="form-check" wire:ignore>
+
     @foreach (collect($options)->unique() as $key => $option)
         <input type="checkbox" name="{{ $name }}[]" value="{{ $option['id'] }}"
             wire:model="form_data.{{ $name }}.{{ $option['id'] }}">
-        <label class="form-check-label"><a href="#"
-                onclick="Livewire.emit('modal.open','modal.consent.description',{'id':'{{ $option['id'] }}'})">{{ $option['name'] }}
+        <label class="form-check-label">
+            {{--  TO-DO Sistemare traduzione su index name --}}
+            <a href="#"
+                onclick="Livewire.emit('modal.open','modal.consent.description',{'id':'{{ $option['id'] }}'})">{{ $option['name']['it'] }}
             </a></label>
         <a href="#" onclick="Livewire.emit('modal.open','modal.consent.description',{'id':'{{ $option['id'] }}'})">
-            <p>{{ substr($option['txt'], 0, 100) }}...</p>
+            <p>{!! substr($option['txt'], 0, 100) !!}...</p>
         </a>
         <br>
     @endforeach
