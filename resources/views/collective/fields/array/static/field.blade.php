4c4679cf984340adf443cc599fa6b6dc46c54a42
Retype GitHub Action <retype@object.net>
2023-05-26 15:21:12 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

---
+++ resources/views/collective/fields/array/static/field.blade.php
@@ -0,0 +1,11 @@
+@php
+    $val = Form::getValueAttribute($name);
+@endphp
+<table class="table table-bordered">
+    @foreach ($val as $k => $v)
+        <tr>
+            <td>{{ $k }}</td>
+            <td>{{ is_string($v) ? $v : '--ARRAY--' }}</td>
+        </tr>
+    @endforeach
+</table>
