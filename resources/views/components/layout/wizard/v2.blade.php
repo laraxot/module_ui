d4f51552fbfcaf7b857924e41ce151c742ac5e5f
Retype GitHub Action <retype@object.net>
2023-05-29 08:36:49 +0000

    Refreshes Retype-generated documentation.
    Process triggered by samueleorfei.

--- resources/views/components/layout/wizard/v2.blade.php
+++ resources/views/components/layout/wizard/v2.blade.php
@@ -1,7 +1,7 @@
 @props([
     'steps' => [],
 ])
-<div class="col-12">
+<div>
     <div class="mb-5">
         <ul class="nav nav-tabs overflow-x border-0">
             @php
@@ -25,11 +25,13 @@
             @endforeach
         </ul>
     </div>
-
     {{ $slot }}
 
+    <x-flash-message />
+
+
     <nav aria-label="wizard navigation">
-        <ul class="pagination d-flex justify-content-between py-4">
+        <ul class="pagination">
             @if ($hasPrevious)
                 <li class="page-item">
                     <a class="page-link" href="#" aria-label="Previous" wire:click="previousStep()">
@@ -47,20 +49,16 @@
                 <li class="page-item">
                     <a class="page-link" href="#" aria-label="Next"
                         wire:click="{{ method_exists($this, 'goNextStep') ? 'goNextStep' : 'nextStep' }}()">
-                        Avanti
                         <span aria-hidden="true">&raquo;</span>
                     </a>
                 </li>
             @else
                 <li class="page-item disabled">
                     <a class="page-link" href="#" aria-label="Next">
-                        Avanti
                         <span aria-hidden="true">&nbsp;</span>
                     </a>
                 </li>
             @endif
         </ul>
     </nav>
-
-    <x-flash-message />
 </div>
