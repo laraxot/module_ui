ef73dfcb33756d32842bb220e8708cf2a9375671
Retype GitHub Action <retype@object.net>
2023-06-01 15:26:04 +0000

    Refreshes Retype-generated documentation.
    Process triggered by samueleorfei.

--- resources/views/layouts/default/admin/home.blade.php
+++ resources/views/layouts/default/admin/home.blade.php
@@ -1,12 +1,12 @@
 @extends('adm_theme::layouts.app')
 @section('content')
+    {{--
     @lang('pub_theme::txt.Welcome.label')
-
+    --}}
 
 
     @foreach ($_panel->getActions('item') as $act)
         <x-button.action :action="$act" />
-        {{-- <x-button.action :action="$act" /> --}}
     @endforeach
 
     {{-- sono le azioni di quel definito Modulo percio' item }
